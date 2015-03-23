26fe648c7b7ef8c8669cae52341f93a2c44c0831
fxl0206 <fxl0206@github.com>
2015-03-23 17:28:00 +0800

    init

---
+++ Makefile
@@ -0,0 +1,4 @@
+PROJECT = grey_bird
+DEPS = cowboy
+dep_cowboy = git https://github.com/ninenines/cowboy master
+include ./erlang.mk
---
+++ src/grey_bird_app.erl
@@ -0,0 +1,25 @@
+%% Feel free to use, reuse and abuse the code in this file.
+
+%% @private
+-module(grey_bird_app).
+-behaviour(application).
+
+%% API.
+-export([start/2]).
+-export([stop/1]).
+
+%% API.
+
+start(_Type, _Args) ->
+	Dispatch = cowboy_router:compile([
+		{'_', [
+			{"/", auth_handler, []}
+		]}
+	]),
+	{ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
+		{env, [{dispatch, Dispatch}]}
+	]),
+	grey_bird_sup:start_link().
+
+stop(_State) ->
+	ok.
