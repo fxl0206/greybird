3b80e6296bd7003df670ffdc1b042b81e8901686
fxl0206 <fxl0206@github.com>
2015-03-26 15:33:58 +0800

    modified:   Makefile 	modified:   src/auth_handler.erl 	modified:   src/grey_bird.app.src

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 PROJECT = grey_bird
 DEPS = cowboy
 dep_cowboy = git https://github.com/ninenines/cowboy master
+DEPS = erlsom
+dep_erlsom=git https://github.com/willemdj/erlsom master
 include ./erlang.mk
--- src/auth_handler.erl
+++ src/auth_handler.erl
@@ -2,9 +2,7 @@
 
 %% @doc GET echo handler.
 -module(auth_handler).
-
 -export([init/2]).
-
 init(Req, Opts) ->
 	Method = cowboy_req:method(Req),
 	HasBody = cowboy_req:has_body(Req),
@@ -24,20 +22,33 @@ maybe_echo(<<"GET">>, false, Req) ->
 		{<<"content-type">>, <<"text/plain; charset=utf-8">>}
 	], Echostr, Req);
 maybe_echo(<<"POST">>, true, Req) ->
-	Token="myluckyfxl",
-	io:format("~p ~n ", [Token]),
-	{ok, PostVals, _} = cowboy_req:body_qs(Req),
-	Signature = proplists:get_value(<<"signature">>, PostVals),
-	Timestamp = proplists:get_value(<<"timestamp">>, PostVals),
-	Nonce = proplists:get_value(<<"nonce">>, PostVals),
-	Echostr = proplists:get_value(<<"echostr">>, PostVals),
-	Tmps = [Token,Timestamp,Nonce],
-	io:format("~p ~n ~p ~n", [Token, Signature]),
-	lists:usort(Tmps),
+	{ok, [{Body,_}], _} = cowboy_req:body_qs(Req),
+	io:format("~ts",[Body]),
+    Xml = binary_to_list(Body),
+	{FromUserName,ToUserName,Content}=xml_parse(Xml),
+	Rep="<xml>
+		<ToUserName><![CDATA["++ToUserName++"]]></ToUserName>
+		<FromUserName><![CDATA["++FromUserName++"]]></FromUserName>
+		<CreateTime>12345678</CreateTime>
+		<MsgType><![CDATA[text]]></MsgType>
+		<Content><![CDATA[自动回复：哈儿 你说了："++Content++"]]></Content>
+		</xml>",
+	io:format("~ts",[Rep]),
 	cowboy_req:reply(200, [
-		{<<"content-type">>, <<"text/plain; charset=utf-8">>}
-	], Echostr, Req);
+		{<<"content-type">>, <<"xml/plain; charset=utf-8">>}
+	], unicode:characters_to_binary(Rep), Req);
 maybe_echo(_, _, Req) ->
 	%% Method not allowed.
 	io:format(" ~p  ~n",["ttt"]),
 	cowboy_req:reply(405, Req).
+xml_parse(Xml)->
+	%io:format("~p ~n ", [code:get_path()]), 
+	io:format("~p ~n ", [Xml]), 
+ 	{Doc, _} =xmerl_scan:string(Xml),
+	%io:format(" ~p  ~n",[Doc]),
+	[{_,_,_,_,Content,_}]=xmerl_xpath:string("/xml/Content/text()", Doc),
+	[{_,_,_,_,FromUserName,_}]=xmerl_xpath:string("/xml/ToUserName/text()", Doc),
+	[{_,_,_,_,ToUserName,_}]=xmerl_xpath:string("/xml/FromUserName/text()", Doc),
+	%[{xmlText,[{'Content',10},{xml,1}],
+    %      1,[],"this is a test",cdata}]
+	{FromUserName,ToUserName,Content}.
