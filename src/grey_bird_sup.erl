04697d858f9dd194fbfb7782325a600c8cc6ae43
fxl <fxl0206@gmail.com>
2015-03-30 11:23:47 +0800

    Update grey_bird_sup.erl

--- src/grey_bird_sup.erl
+++ src/grey_bird_sup.erl
@@ -19,6 +19,10 @@ start_link() ->
 %% supervisor.
 
 init([]) ->
-	Procs = [{mysql, {mysql, start_link, [conn,"127.0.0.1",undefined,"root","286955","greybird",utf8]},permanent, brutal_kill, worker, [mysql]}],
+        PoolOptions  = [{size, 10}, {max_overflow, 20}],
+    MySqlOptions = [{user, "root"}, {password, "286955"}, {database, "greybird"},
+                    {prepare, [{wx_msg, "SELECT * FROM wx_msg WHERE seq=?"}]}],
+        Procs=mysql_poolboy:child_spec(pool1, PoolOptions, MySqlOptions),
+	%Procs = [{mysql, {mysql, start_link, [conn,"127.0.0.1",undefined,"root","286955","greybird",utf8]},permanent, brutal_kill, worker, [mysql]}],
 	%Procs=[],
 	{ok, {{one_for_one, 10, 10}, Procs}}.
